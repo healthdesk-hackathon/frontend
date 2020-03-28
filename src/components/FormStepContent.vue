0eb31941b28249bddbda06508c5d3fab53a6966d
Pierre <p.monico@gmx.fr>
2020-03-28 14:09:00 +0100

    General styling of the form steps

---
+++ src/components/FormStepContent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="form-step">
+    <h1 class="title has-text-centered">{{ title }}</h1>
+    <div class="has-text-centered">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { required: true, type: String }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.form-step {
+  padding: 3rem 1rem;
+}
+</style>
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,24 +1,59 @@
 <template>
-  <b-steps v-model="activeStep" :animated="true" :has-navigation="true">
-    <b-step-item label="Account">
-      <h1 class="title has-text-centered">Account</h1>
-      Lorem ipsum dolor sit amet.
+  <b-steps v-model="activeStep" :animated="true" :has-navigation="false">
+    <b-step-item label="Personal data" icon="account">
+      <FormStepContent title="Personal data">
+        Your personal data
+      </FormStepContent>
     </b-step-item>
 
-    <b-step-item label="Profile">
-      <h1 class="title has-text-centered">Profile</h1>
-      Lorem ipsum dolor sit amet.
+    <b-step-item label="Symptoms" icon="alert-circle-outline">
+      <FormStepContent title="Symptoms">
+        Your symptoms
+      </FormStepContent>
     </b-step-item>
 
-    <b-step-item label="Finish">
-      <h1 class="title has-text-centered">Finish</h1>
-      Lorem ipsum dolor sit amet.
+    <b-step-item label="Additional information" icon="plus">
+      <FormStepContent title="Additional information">
+        Your personal data
+      </FormStepContent>
     </b-step-item>
+
+    <b-step-item label="Finish" icon="check">
+      <FormStepContent title="Finish">
+        Finish the whole thing
+      </FormStepContent>
+    </b-step-item>
+    <template slot="navigation" slot-scope="{ previous, next }">
+      <div class="has-text-centered">
+        <b-button
+          expanded
+          rounded
+          class="has-text-weight-bold"
+          type="is-primary is-large"
+          :disabled="next.disabled"
+          @click.prevent="next.action"
+        >
+          Next step</b-button
+        ><br />
+        <b-button
+          type="is-text is-small"
+          :disabled="previous.disabled"
+          @click.prevent="previous.action"
+        >
+          Previous step
+        </b-button>
+      </div>
+    </template>
   </b-steps>
 </template>
 
 <script>
+import FormStepContent from "@/components/FormStepContent.vue";
+
 export default {
+  components: {
+    FormStepContent
+  },
   data() {
     return {
       activeStep: 0
