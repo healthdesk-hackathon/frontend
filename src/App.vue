630d43af34216f45c02a0374502c64591bfc16e2
Pierre <p.monico@gmx.fr>
2020-03-28 11:32:32 +0100

    Install and configure buefy

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin: auto;
-  width: 50%;
-  text-align: center;
-}
-</style>
+<style lang="scss"></style>
