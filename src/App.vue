efd413fd92bc41768ac55ba34b7657485e2adad0
Pierre <p.monico@gmx.fr>
2020-03-28 11:51:10 +0100

    Configures theming and sets it up according to layout

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,14 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+// All custom theme variables are prepended automatically to every component (and thus usable in there without an import).
+// Check vue.config.js and styles/theme.scss. Order below matters.
+
+// Import custom styles
+@import "@/assets/styles/styles.scss";
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+</style>
