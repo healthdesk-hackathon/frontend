75fc3da4f096c66dd3c5bded5281fb5f5b0779bf
Pierre <p.monico@gmx.fr>
2020-04-04 14:47:02 +0200

    Expose bed types in store

--- src/store/beds.js
+++ src/store/beds.js
@@ -1,13 +1,16 @@
 import { API } from "@/service/api";
 
-const BED_ENDPOINT = "beds";
+const BED_ENDPOINT = "bed";
+const BEDTYPE_ENDPOINT = "bed-type";
 
 const MUTATIONS = {
-  SET_BEDS: "SET_BEDS"
+  SET_BEDS: "SET_BEDS",
+  SET_BEDTYPES: "SET_BEDTYPES"
 };
 
 const state = {
-  beds: []
+  beds: [],
+  bedtypes: []
 };
 
 const getters = {};
@@ -15,13 +18,42 @@ const getters = {};
 const mutations = {
   [MUTATIONS.SET_BEDS](state, beds) {
     state.beds = beds;
+  },
+  [MUTATIONS.SET_BEDTYPES](state, bedtypes) {
+    state.bedtypes = bedtypes;
   }
 };
 
 const actions = {
-  /**
-   * Getting all beds
-   */
+  // Bed types
+  async fetchBedtypes({ commit }) {
+    try {
+      const response = await API.service.get(BEDTYPE_ENDPOINT + "/");
+      commit(MUTATIONS.SET_BEDS, response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  async updateBedtype({ dispatch }, bedType) {
+    try {
+      await API.service.put(`${BEDTYPE_ENDPOINT}/${bedType.id}`, bedType);
+      await dispatch("fetchBedtypes");
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  async deleteBedtype({ dispatch }, bedType) {
+    try {
+      await API.service.delete(`${BEDTYPE_ENDPOINT}/${bedType.id}`);
+      await dispatch("fetchBedtypes");
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  // Beds
   async fetchBeds({ commit }) {
     try {
       const response = await API.service.get(BED_ENDPOINT + "/");
@@ -31,9 +63,6 @@ const actions = {
     }
   },
 
-  /**
-   * Updates a bed and refreshes the list
-   */
   async updateBed({ dispatch }, bed) {
     try {
       await API.service.put(`${BED_ENDPOINT}/${bed.id}`, bed);
@@ -43,9 +72,6 @@ const actions = {
     }
   },
 
-  /**
-   * Deletes bed and refreshes the list
-   */
   async deleteBed({ dispatch }, bed) {
     try {
       await API.service.delete(`${BED_ENDPOINT}/${bed.id}`);
