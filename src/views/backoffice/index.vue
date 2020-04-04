9e5a864a0672382e045eb00aba1755caedb28628
Pierre <p.monico@gmx.fr>
2020-04-04 12:21:07 +0200

    Include navigation bar

--- src/views/backoffice/index.vue
+++ src/views/backoffice/index.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="columns">
-    <div class="column is-narrow" id="menu-column">
-      <section class="section">
-        <TheSideBar />
-      </section>
-    </div>
-    <div class="column">
-      <section class="section">
-        <router-view />
-      </section>
+  <div>
+    <TheNavigation />
+    <div class="columns">
+      <div class="column is-narrow" id="menu-column">
+        <!-- TODO: fix this column on scroll -->
+        <section class="section">
+          <TheSideBar />
+        </section>
+      </div>
+      <div class="column">
+        <section class="section">
+          <router-view />
+        </section>
+      </div>
     </div>
   </div>
 </template>
@@ -16,10 +20,12 @@
 <script>
 import { mapActions } from "vuex";
 import TheSideBar from "@/components/Dashboard/TheSidebar";
+import TheNavigation from "@/components/Dashboard/TheNavigation";
 
 export default {
   components: {
-    TheSideBar
+    TheSideBar,
+    TheNavigation
   },
   computed: {
     things() {
