fe41117c9a2dcb5653e6e2fb70d0aef3bdbd7981
Pierre <p.monico@gmx.fr>
2020-04-05 13:50:06 +0200

    Move titles into sections

--- src/views/backoffice/bedmanagement/BedList.vue
+++ src/views/backoffice/bedmanagement/BedList.vue
@@ -1,72 +1,70 @@
 <template>
-  <div>
+  <section class="section">
     <h1 class="title is-2">
       Beds
     </h1>
-    <section class="section">
-      <b-table
-        :loading="!beds"
-        :data="beds"
-        default-sort="id"
-        hoverable
-        paginated
-      >
-        <template slot-scope="props">
-          <b-table-column
-            width="40"
-            label="ID"
-            field="id"
-            numeric
-            sortable
-            searchable
-          >
-            {{ props.row.id }}
-          </b-table-column>
-          <b-table-column label="Bed type" field="bed_type">
-            {{ props.row.bed_type_display }}
-          </b-table-column>
-          <b-table-column label="State" field="state">
-            {{ props.row.state_display }}
-          </b-table-column>
-          <b-table-column label="Reason" field="reason">
-            {{ props.row.reason_display }}
-          </b-table-column>
-          <b-table-column label="Actions">
-            <div class="buttons">
-              <b-button
-                v-if="props.row.state !== 0"
-                @click="setUnavailable(props.row)"
-                size="is-small"
-              >
-                Unavailable
-              </b-button>
-              <b-button
-                v-if="props.row.state === 0"
-                @click="setAvailable(props.row)"
-                size="is-small"
-              >
-                Available
-              </b-button>
-              <b-button
-                v-if="props.row.state !== 0"
-                @click="setEquipmentFailure(props.row)"
-                size="is-small"
-              >
-                Equipment failure
-              </b-button>
-              <b-button
-                v-if="props.row.state !== 0"
-                @click="setCleaning(props.row)"
-                size="is-small"
-              >
-                Cleaning
-              </b-button>
-            </div>
-          </b-table-column>
-        </template>
-      </b-table>
-    </section>
-  </div>
+    <b-table
+      :loading="!beds"
+      :data="beds"
+      default-sort="id"
+      hoverable
+      paginated
+    >
+      <template slot-scope="props">
+        <b-table-column
+          width="40"
+          label="ID"
+          field="id"
+          numeric
+          sortable
+          searchable
+        >
+          {{ props.row.id }}
+        </b-table-column>
+        <b-table-column label="Bed type" field="bed_type">
+          {{ props.row.bed_type_display }}
+        </b-table-column>
+        <b-table-column label="State" field="state">
+          {{ props.row.state_display }}
+        </b-table-column>
+        <b-table-column label="Reason" field="reason">
+          {{ props.row.reason_display }}
+        </b-table-column>
+        <b-table-column label="Actions">
+          <div class="buttons">
+            <b-button
+              v-if="props.row.state !== 0"
+              @click="setUnavailable(props.row)"
+              size="is-small"
+            >
+              Unavailable
+            </b-button>
+            <b-button
+              v-if="props.row.state === 0"
+              @click="setAvailable(props.row)"
+              size="is-small"
+            >
+              Available
+            </b-button>
+            <b-button
+              v-if="props.row.state !== 0"
+              @click="setEquipmentFailure(props.row)"
+              size="is-small"
+            >
+              Equipment failure
+            </b-button>
+            <b-button
+              v-if="props.row.state !== 0"
+              @click="setCleaning(props.row)"
+              size="is-small"
+            >
+              Cleaning
+            </b-button>
+          </div>
+        </b-table-column>
+      </template>
+    </b-table>
+  </section>
 </template>
 
 <script>
