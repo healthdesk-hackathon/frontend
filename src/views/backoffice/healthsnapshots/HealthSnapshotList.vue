4e21af3d742693c90107ce1cdf15db670bb07339
Phil <phil.ayres@consected.com>
2020-04-05 14:57:34 +0100

    Make prop optional

--- src/views/backoffice/healthsnapshots/HealthSnapshotList.vue
+++ src/views/backoffice/healthsnapshots/HealthSnapshotList.vue
@@ -1,33 +1,24 @@
 <template>
   <div>
     <section class="section">
-      <h1 class="title is-2">
-        Health Snapshots
-      </h1>
-      <b-table
-        :data="healthSnapshots"
-        :default-sort="['created_at', 'desc']"
-        hoverable
-        paginated
-      >
+      <h1 class="title is-2">Health Snapshots</h1>
+      <b-table :data="healthSnapshots" :default-sort="['created_at', 'desc']" hoverable paginated>
         <template slot-scope="props">
-          <b-table-column field="created_at" label="Date/Time" sortable>
-            {{ props.row.created_at | datetime }}
-          </b-table-column>
+          <b-table-column
+            field="created_at"
+            label="Date/Time"
+            sortable
+          >{{ props.row.created_at | datetime }}</b-table-column>
 
-          <b-table-column field="admission" label="Admission" searchable>
-            {{ props.row.admission }}
-          </b-table-column>
+          <b-table-column field="admission" label="Admission" searchable>{{ props.row.admission }}</b-table-column>
 
-          <b-table-column field="severity" label="Severity" sortable>
-            {{ props.row.severity }}
-          </b-table-column>
+          <b-table-column field="severity" label="Severity" sortable>{{ props.row.severity }}</b-table-column>
         </template>
         <template slot="empty">
           <section class="section">
             <div class="content has-text-grey has-text-centered">
               <p>
-                <b-icon icon="emoticon-sad" size="is-large"> </b-icon>
+                <b-icon icon="emoticon-sad" size="is-large"></b-icon>
               </p>
               <p>Nothing here.</p>
             </div>
@@ -42,7 +33,9 @@
 import { mapActions, mapState } from "vuex";
 
 export default {
-  props: ["admission_id"],
+  props: {
+    admission_id: { type: String, required: false, default: null }
+  },
   data() {
     return {};
   },
