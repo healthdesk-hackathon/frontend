eebe133266bef82b226251887458008a55ddee12
Pierre <p.monico@gmx.fr>
2020-04-05 14:10:23 +0200

    Beautify healthsnaphot table

--- src/views/backoffice/healthsnapshots/HealthSnapshotList.vue
+++ src/views/backoffice/healthsnapshots/HealthSnapshotList.vue
@@ -4,7 +4,21 @@
       <h1 class="title is-2">
         Health Snapshots
       </h1>
-      <b-table :data="healthSnapshots" :columns="columns"></b-table>
+      <b-table :data="healthSnapshots">
+        <template slot-scope="props">
+          <b-table-column field="created_at" label="Date/Time" sortable>
+            {{ props.row.created_at | datetime }}
+          </b-table-column>
+
+          <b-table-column field="admission" label="Admission" searchable>
+            {{ props.row.admission }}
+          </b-table-column>
+
+          <b-table-column field="severity" label="Severity" sortable>
+            {{ props.row.severity }}
+          </b-table-column>
+        </template>
+      </b-table>
     </section>
   </div>
 </template>
@@ -14,22 +28,7 @@ import { mapActions, mapState } from "vuex";
 
 export default {
   data() {
-    return {
-      columns: [
-        {
-          field: "created_at",
-          label: "Date/Time"
-        },
-        {
-          field: "admission",
-          label: "Admission"
-        },
-        {
-          field: "severity",
-          label: "Severity"
-        }
-      ]
-    };
+    return {};
   },
   computed: {
     ...mapState("healthsnapshots", ["healthSnapshots"])
